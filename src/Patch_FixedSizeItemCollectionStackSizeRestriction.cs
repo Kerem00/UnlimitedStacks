3cf170f3e8e11242b92c1e17540b45a0872efe51
Kerem00 <kerembicen94@gmail.com>
2026-07-09 17:56:52 +0300

    source files

---
+++ src/Patch_FixedSizeItemCollectionStackSizeRestriction.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Opsive.UltimateInventorySystem.Core;
+using SunnySideUp;
+
+namespace UnlimitedStacks
+{
+    [HarmonyPatch(typeof(FixedSizeItemCollectionStackSizeRestriction), "GetMaxStackSize")]
+    public static class Patch_FixedSizeItemCollectionStackSizeRestriction
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(
+            FixedSizeItemCollectionStackSizeRestriction __instance,
+            ItemDefinition item,
+            ref int __result)
+        {
+            if (item == null)
+            {
+                __result = 0;
+                return false;
+            }
+
+            __result = 9999;
+            return false;
+        }
+    }
+}
---
+++ src/UnlimitedStacksMod.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace UnlimitedStacks
+{
+    [BepInPlugin("lwitw.mods.UnlimitedStacks", "Raises stack count to 9999", "1.0.0")]
+    public class UnlimitedStacksMod : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new Harmony("lwitw.mods.UnlimitedStacks").PatchAll();
+            this.Logger.LogInfo((object)"Raises stack count to 9999");
+        }
+    }
+}
